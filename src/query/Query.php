9b4ede43d932cc24e778ea35d94f49b4e8106b03
NicoRiri <nicolate.bernardet@gmail.com>
2023-11-14 16:26:06 +0100

    fix: correction get delete insert

--- src/query/Query.php
+++ src/query/Query.php
@@ -42,14 +42,26 @@ class Query
         $stmt->execute($this->args);
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
-public function insert([] $args):Query{
-    $this->sql = 'insert into ' . $this->sqltable . ' values (';
-    $this->sql .= implode(',', array_fill(0, count($args), '?'));
-    $this->sql .= ')';
-    $this->args = $args;
-    return $this;
+    public function insert([] $args):Array{
+        $ini = parse_ini_file(__DIR__ . "/../db.ini");
+        $pdo = new PDO($ini["driver"].":host=".$ini["host"].";dbname=".$ini["dbname"], $ini["user"], $ini["password"]);
+        $this->sql = 'insert into ' . $this->sqltable . ' values (';
+        $this->sql .= implode(',', array_fill(0, count($args), '?'));
+        $this->sql .= ')';
+        $this->args = $args;
+        $stmt = $pdo->prepare($this->sql);
+        $stmt->execute($this->args);
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
 
-}
+    public function delete():Query{
+        $ini = parse_ini_file(__DIR__ . "/../db.ini");
+        $pdo = new PDO($ini["driver"].":host=".$ini["host"].";dbname=".$ini["dbname"], $ini["user"], $ini["password"]);
+        $this->sql = 'delete FROM ? where ?';
+        $stmt = $pdo->prepare($this->sql);
+        $stmt->execute([$this->sqltable, $this->where]);
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
 
 
 
