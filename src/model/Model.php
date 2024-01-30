67268946a49da72765482ab97c723c2fa67f7a97
oudin-clement <clementoudin3@gmail.com>
2024-01-30 19:10:14 +0100

    feat: gestion des associations

--- src/model/Model.php
+++ src/model/Model.php
@@ -17,11 +17,35 @@ abstract class Model
     {
         if (array_key_exists($name, $this->_atts)) {
             return $this->_atts[$name];
+        }elseif (method_exists($this, $name)) {
+            return $this->$name();
         }
     }
 
     public function __set(string $name, mixed $val): void
     {
         $this->atts[$name] = $val;
+    }
+
+    public function belongs_to(string $model, string $foreign_key = null): Model
+    {
+        if (is_null($foreign_key)) {
+            $foreign_key = strtolower($model) . '_id';
+        }
+        $class = 'iutnc\\hellokant\\model\\' . ucfirst($model);
+        $obj = new $class();
+        $this->atts[$model] = $obj->find([[$obj::$idColumn, '=', $this->atts[$foreign_key]]]);
+        return $this;
+    }
+
+    public function has_many(string $model, string $foreign_key = null): Model
+    {
+        if (is_null($foreign_key)) {
+            $foreign_key = strtolower(get_class($this)) . '_id';
+        }
+        $class = 'iutnc\\hellokant\\model\\' . ucfirst($model);
+        $obj = new $class();
+        $this->atts[$model] = $obj->find([[$foreign_key, '=', $this->atts[$obj::$idColumn]]]);
+        return $this;
     }
 }
